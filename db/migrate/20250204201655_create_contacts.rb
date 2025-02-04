869b109442913f00cac2db57e9917dc27dc47a05
Austin Tang <austin.tang@kellogg.northwestern.edu>
2025-02-04 20:44:24 +0000

    in class edits

---
+++ db/migrate/20250204201655_create_contacts.rb
@@ -0,0 +1,12 @@
+class CreateContacts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :contacts do |t|
+      t.string "first name"
+      t.string "last name"
+      t.string "email"
+      t.integer "company_id"
+
+      t.timestamps
+    end
+  end
+end
